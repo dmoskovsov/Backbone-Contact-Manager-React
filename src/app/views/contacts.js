140efccfcfc142d7f5a65f7868becc68958c963e
daniil.moskovtsov <daniil.moskovtsov@dsquare.be>
2015-01-26 00:08:13 +0100

    #6 Get rid of hardcoded contacts and use local storage instead. Done. Adding contact filter

--- src/app/views/contacts.js
+++ src/app/views/contacts.js
@@ -1,22 +1,43 @@
 'use strict';
 var React = require('react'),
-    ContactView = require('./contact.js');
+    ContactView = require('./contact.js'),
+    mui = require('material-ui'),
+    _ = require('underscore'),
+    Input = mui.Input;
 
 var ContactsView = React.createClass({
     getInitialState: function () {
-        return {contacts: this.props.contacts.models};
+        return {contacts: this.props.contacts.models, searchString: ''};
+    },
+    filterContacts: function () {
+        this.setState({contacts: this.state.contacts, searchString: event.target.value});
     },
     render: function () {
-        var contacts = this.state.contacts.map(function (contact) {
+        var contacts = this.getFilteredComponents().map(function (contact) {
             return <ContactView key={contact.id}
-                    contact={contact}/>;
+                contact={contact}/>
         }, this);
 
         return <div>
-            <ul className="media-list row">
+            <div className='search-container'>
+                <input type="text"
+                    className='search-input'
+                    name="filterInput"
+                    onChange={this.filterContacts}/>
+            </div>
+
+            <ul className="row">
                     {contacts}
             </ul>
         </div>
+    },
+    getFilteredComponents: function () {
+        var searchString = this.state.searchString.toLowerCase();
+        return _.filter(this.props.contacts.models, function (contact) {
+            var name = contact.attributes.name.toLowerCase();
+            var lastName = contact.attributes.lastName.toLowerCase();
+            return (name.toLowerCase().indexOf(searchString) !== -1 || lastName.indexOf(searchString) !== -1);
+        });
     }
 });
 module.exports = ContactsView;
